24c85085d28215c341dd8efb30345dde85d1c597
Steven Suing <Steven.suing@gmail.com>
2013-05-01 15:38:54 -0400

    Not sure about this one

---
+++ Model/Model/PolicyChangeModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Model
+{
+    class PolicyChangeCommand
+    {
+        public int PolicyId { get; set; }
+        public int CustomerId { get; set; }
+
+        List<Vehicle> VehiclesAdded { get; set; }
+        List<Vehicle> VehiclesDeleted { get; set; }
+        List<Vehicle> VehiclesUpdated { get; set; }
+
+        List<Coverage> CoveragesAdded { get; set; }
+        List<Coverage> CoveragesDeleted { get; set; }
+        List<Coverage> CoveragesUpdated { get; set; }
+
+        DVA DVA { get; set; }
+    }
+
+    public class Vehicle { }
+
+    public class DVA { }
+    public class Coverage { }
+
+}
