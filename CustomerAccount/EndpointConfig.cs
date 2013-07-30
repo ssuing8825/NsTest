108b822e86f9cc643e7dc9fe0453088a111ecbdc
Steven Suing <Steven.suing@gmail.com>
2013-07-30 11:41:39 -0400

    Added NHybernate and connection, Doesn't work

--- CustomerAccount/EndpointConfig.cs
+++ CustomerAccount/EndpointConfig.cs
@@ -1,16 +1,24 @@
 
 
 
+using NHibernate.Cfg;
+
 namespace CustomerAccountSystem
 {
     using NServiceBus;
 
-	/*
-		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
-		can be found here: http://nservicebus.com/GenericHost.aspx
-	*/
-    public class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher, UsingTransport<NServiceBus.RabbitMQ>
-    { 
+    /*
+        This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
+        can be found here: http://nservicebus.com/GenericHost.aspx
+    */
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Publisher, UsingTransport<NServiceBus.RabbitMQ>, IWantCustomInitialization
+    {
+        public void Init()
+        {
+            Configure.With()
+                  .DefaultBuilder()
+                   .UseNHibernateSagaPersister();
+        }
     }
 
 }
